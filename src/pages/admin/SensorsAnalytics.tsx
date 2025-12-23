c6f0234381ca55e97ef986bc6f8aabdb1bcbb3c6
Felix Sudrajat <felixangga077@gmail.com>
2025-12-23 13:59:44 +0700

    feat: Add sensor analytics page with data visualization, statistics, and CSV export, updating dependencies.

--- src/pages/admin/SensorsAnalytics.tsx
+++ src/pages/admin/SensorsAnalytics.tsx
@@ -65,7 +65,6 @@ export default function SensorsAnalytics(): JSX.Element {
 
     useEffect(() => {
         fetchData();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [timeRange, currentPage]);
 
     const handleExportCSV = () => {
